8b876aecfe216e9b6014babac88b7bc01c6aeb6f
Asad <asad.chaudhry@ztbl.com.pk>
2025-06-17 13:30:38 +0500

    Model & DBConnection updated

--- AIS/AIS/Models/AISPostComplianceModel.cs
+++ AIS/AIS/Models/AISPostComplianceModel.cs
@@ -2,34 +2,34 @@
     {
     public class AISPostComplianceModel
         {
-        public string P_NAME { get; set; }
-        public string C_NAME { get; set; }
-        public string COM_ID { get; set; }
-        public string OLD_PARA_ID { get; set; }
-        public string NEW_PARA_ID { get; set; }
-        public string AUDIT_PERIOD { get; set; }
-        public string ENTITY_ID { get; set; }
-        public string ENTITY_CODE { get; set; }
-        public string AUDITED_BY { get; set; }
-        public string ENTITY_TYPE_ID { get; set; }
-        public string COM_CYCLE { get; set; }
-        public string COM_STATUS { get; set; }
-        public string COM_STAGE { get; set; }
-        public string PARA_STATUS { get; set; }
-        public string PARA_NO { get; set; }
-        public string GIST_OF_PARAS { get; set; }
-        public string SETTELED_ON { get; set; }
-        public string SETTELED_BY { get; set; }
+        public string PNAME { get; set; }
+        public string CNAME { get; set; }
+        public string COMID { get; set; }
+        public string OLDPARAID { get; set; }
+        public string NEWPARAID { get; set; }
+        public string AUDITPERIOD { get; set; }
+        public string ENTITYID { get; set; }
+        public string ENTITYCODE { get; set; }
+        public string AUDITEDBY { get; set; }
+        public string ENTITYTYPEID { get; set; }
+        public string COMCYCLE { get; set; }
+        public string COMSTATUS { get; set; }
+        public string COMSTAGE { get; set; }
+        public string PARASTATUS { get; set; }
+        public string PARANO { get; set; }
+        public string GISTOFPARAS { get; set; }
+        public string SETTELEDON { get; set; }
+        public string SETTELEDBY { get; set; }
         public string IND { get; set; }
-        public string PARA_ADDED_ON { get; set; }
-        public string CAU_STATUS { get; set; }
-        public string CAU_ASSIGNED_ENT_ID { get; set; }
-        public string BR_RESPONSE_BY { get; set; }
-        public string BR_RESPONSE_ON { get; set; }
-        public string CAU_ASSIGNED_BY { get; set; }
-        public string CAU_ASSIGNED_ON { get; set; }
-        public string SETTLEMENT_COM_REVIEWED_BY { get; set; }
-        public string SETTLEMENT_COM_REVIEWED_ON { get; set; }
+        public string PARAADDEDON { get; set; }
+        public string CAUSTATUS { get; set; }
+        public string CAUASSIGNEDENTID { get; set; }
+        public string BRRESPONSEBY { get; set; }
+        public string BRRESPONSEON { get; set; }
+        public string CAUASSIGNEDBY { get; set; }
+        public string CAUASSIGNEDON { get; set; }
+        public string SETTLEMENTCOMREVIEWEDBY { get; set; }
+        public string SETTLEMENTCOMREVIEWEDON { get; set; }
         public string RISK { get; set; }
         public string ANNEX { get; set; }
         }
