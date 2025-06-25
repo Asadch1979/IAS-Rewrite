62f4c0c9f388f0350738ba3803208bc5e27cdfb2
Asadch1979 <asadali3000@hotmail.com>
2025-06-25 18:52:04 +0500

    Implement SBP compliance DB and API

---
+++ AIS/AIS/Controllers/Compliance/SBPComplianceController.cs
@@ -0,0 +1,111 @@
+using AIS.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AIS.Controllers.Compliance
+{
+    public class SBPComplianceController : Controller
+    {
+        private readonly ILogger<SBPComplianceController> _logger;
+        private readonly TopMenus tm;
+        private readonly SessionHandler sessionHandler;
+        private readonly DBConnection dBConnection;
+
+        public SBPComplianceController(ILogger<SBPComplianceController> logger,
+            SessionHandler _sessionHandler, DBConnection _dbCon, TopMenus _tpMenu)
+        {
+            _logger = logger;
+            sessionHandler = _sessionHandler;
+            dBConnection = _dbCon;
+            tm = _tpMenu;
+        }
+
+        [HttpGet("Complaince/SBPCompliance")]
+        public IActionResult Index()
+        {
+            ViewData["TopMenu"] = tm.GetTopMenus();
+            ViewData["TopMenuPages"] = tm.GetTopMenusPages();
+            if (!sessionHandler.IsUserLoggedIn())
+                return RedirectToAction("Index", "Login");
+            else
+                return View("../Complaince/SBPCompliance/Index");
+        }
+
+        [HttpGet("Complaince/SBPCompliance/AddObservation")]
+        public IActionResult AddObservation()
+        {
+            ViewData["TopMenu"] = tm.GetTopMenus();
+            ViewData["TopMenuPages"] = tm.GetTopMenusPages();
+            if (!sessionHandler.IsUserLoggedIn())
+                return RedirectToAction("Index", "Login");
+            else
+                return View("../Complaince/SBPCompliance/AddObservation");
+        }
+
+        [HttpGet("Complaince/SBPCompliance/AssignDivision")]
+        public IActionResult AssignDivision()
+        {
+            ViewData["TopMenu"] = tm.GetTopMenus();
+            ViewData["TopMenuPages"] = tm.GetTopMenusPages();
+            if (!sessionHandler.IsUserLoggedIn())
+                return RedirectToAction("Index", "Login");
+            else
+                return View("../Complaince/SBPCompliance/AssignDivision");
+        }
+
+        [HttpGet("Complaince/SBPCompliance/AssignDepartment")]
+        public IActionResult AssignDepartment()
+        {
+            ViewData["TopMenu"] = tm.GetTopMenus();
+            ViewData["TopMenuPages"] = tm.GetTopMenusPages();
+            if (!sessionHandler.IsUserLoggedIn())
+                return RedirectToAction("Index", "Login");
+            else
+                return View("../Complaince/SBPCompliance/AssignDepartment");
+        }
+
+        [HttpGet("Complaince/SBPCompliance/EnterResponse")]
+        public IActionResult EnterResponse()
+        {
+            ViewData["TopMenu"] = tm.GetTopMenus();
+            ViewData["TopMenuPages"] = tm.GetTopMenusPages();
+            if (!sessionHandler.IsUserLoggedIn())
+                return RedirectToAction("Index", "Login");
+            else
+                return View("../Complaince/SBPCompliance/EnterResponse");
+        }
+
+        [HttpGet("Complaince/SBPCompliance/ReviewResponse")]
+        public IActionResult ReviewResponse()
+        {
+            ViewData["TopMenu"] = tm.GetTopMenus();
+            ViewData["TopMenuPages"] = tm.GetTopMenusPages();
+            if (!sessionHandler.IsUserLoggedIn())
+                return RedirectToAction("Index", "Login");
+            else
+                return View("../Complaince/SBPCompliance/ReviewResponse");
+        }
+
+        [HttpGet("Complaince/SBPCompliance/ForwardToCompliance")]
+        public IActionResult ForwardToCompliance()
+        {
+            ViewData["TopMenu"] = tm.GetTopMenus();
+            ViewData["TopMenuPages"] = tm.GetTopMenusPages();
+            if (!sessionHandler.IsUserLoggedIn())
+                return RedirectToAction("Index", "Login");
+            else
+                return View("../Complaince/SBPCompliance/ForwardToCompliance");
+        }
+
+        [HttpGet("Complaince/SBPCompliance/ReviewHistory")]
+        public IActionResult ReviewHistory()
+        {
+            ViewData["TopMenu"] = tm.GetTopMenus();
+            ViewData["TopMenuPages"] = tm.GetTopMenusPages();
+            if (!sessionHandler.IsUserLoggedIn())
+                return RedirectToAction("Index", "Login");
+            else
+                return View("../Complaince/SBPCompliance/ReviewHistory");
+        }
+    }
+}
