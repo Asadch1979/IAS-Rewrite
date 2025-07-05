87e5cab1017b0376dfff88b78ea471f7e5b52487
Asad <asad.chaudhry@ztbl.com.pk>
2025-07-05 14:10:28 +0500

    FadDeskOfficerRptModel

--- AIS/AIS/Models/CircularModel.cs
+++ AIS/AIS/Models/CircularModel.cs
@@ -8,7 +8,7 @@ namespace AIS.Models
         public string DisplayText { get; set; }
         public string Keywords { get; set; }
         public string RedirectedPage { get; set; }
-        public int Division { get; set; }
+        public string Division { get; set; }
         public string ReferenceNo { get; set; }
         public DateTime? IssueDate { get; set; }
         public string DocType { get; set; }
