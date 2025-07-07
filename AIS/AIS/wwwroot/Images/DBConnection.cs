58a48e212c0e900bb9dd3edcca810fd014172696
Asad <asad.chaudhry@ztbl.com.pk>
2025-07-07 10:44:12 +0500

    Delete DBConnection.cs

--- AIS/AIS/wwwroot/Images/DBConnection.cs
+++
@@ -1,6 +0,0 @@
-        public string ReviewerAddChangeStatusRequestForSettledPara(string REFID, string IND, string REMARKS, string Action_IND)
-                cmd.Parameters.Add("P_IND", OracleDbType.Varchar2).Value = IND;
-        public string AuthorizerAddChangeStatusRequestForSettledPara(string REFID, string IND, int NEW_STATUS, string REMARKS, string Action_IND)
-                cmd.Parameters.Add("P_IND", OracleDbType.Varchar2).Value = IND;
-        public string AddAuthorizeChangeStatusRequestForSettledPara(string REFID, string OBS_ID, string IND, string Action_IND)
-                cmd.Parameters.Add("P_IND", OracleDbType.Varchar2).Value = IND;
