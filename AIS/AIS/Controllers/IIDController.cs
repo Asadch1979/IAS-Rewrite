f1fc304277d8765b53e8eca2339aeff0d0ef9c96
Asadch1979 <asadali3000@hotmail.com>
2025-07-15 23:30:57 -0700

    Add LocationType selection for complaints

--- AIS/AIS/Controllers/IIDController.cs
+++ AIS/AIS/Controllers/IIDController.cs
@@ -22,8 +22,8 @@ namespace AIS.Controllers
             {
             ViewData["TopMenu"] = tm.GetTopMenus();
             ViewData["TopMenuPages"] = tm.GetTopMenusPages();
-            ViewData["BranchList"] = dBConnection.GetBranches();
             ViewData["RegionList"] = dBConnection.GetGMsList();
+            ViewData["GMOfficeList"] = dBConnection.GetGMOffices();
             if (!sessionHandler.IsUserLoggedIn())
                 return RedirectToAction("Index", "Login");
             return View("../IID/SubmitComplaint");
