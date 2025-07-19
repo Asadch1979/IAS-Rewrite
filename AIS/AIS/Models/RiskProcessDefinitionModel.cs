130f024898c65b748d5c0aa753f3c7ea2cc777e6
Asadch1979 <asadali3000@hotmail.com>
2025-07-19 07:45:07 -0700

    Remove unused model classes

--- AIS/AIS/Models/RiskProcessDefinitionModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AIS.Models
-    {
-    public class RiskProcessDefinition
-        {
-        public int P_ID { get; set; }
-        public string P_NAME { get; set; }
-        public int RISK_ID { get; set; }
-        }
-    }
