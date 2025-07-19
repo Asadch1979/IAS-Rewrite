1f37d8effaee122946a68fc9dd38656cd03efb59
Asadch1979 <asadali3000@hotmail.com>
2025-07-19 11:54:49 -0700

    Include link id in reference update

--- AIS/AIS/Models/ParaReferenceDataModel.cs
+++ AIS/AIS/Models/ParaReferenceDataModel.cs
@@ -7,5 +7,9 @@ namespace AIS.Models
         public string ParaText { get; set; }
         public List<int> References { get; set; }
         public List<AuditChecklistAnnexureCircularModel> ReferenceDetails { get; set; }
+        /// <summary>
+        /// Raw link records for the para.
+        /// </summary>
+        public List<ParaReferenceLinkModel> ReferenceLinks { get; set; }
     }
 }
