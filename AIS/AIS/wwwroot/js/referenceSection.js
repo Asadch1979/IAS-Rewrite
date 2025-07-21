d88ef9222ccf5d123a4db12d7de1db9e506eb0b2
Asadch1979 <asadali3000@hotmail.com>
2025-07-21 03:59:21 -0700

    Add reference section integration

---
+++ AIS/AIS/wwwroot/js/referenceSection.js
@@ -0,0 +1,156 @@
+function initReferenceSection(comId, readOnly) {
+    var refs = [];
+    var refDetails = [];
+    var refLinks = [];
+    var manualCounter = -1;
+
+    if (readOnly) {
+        $('#searchSection, #saveBtn').hide();
+    } else {
+        $('#searchSection, #saveBtn').show();
+    }
+
+    $('#resultTbl tbody').empty();
+    $('#refList').empty();
+
+    $.get(g_asiBaseURL + '/ApiCalls/GetParaReferenceData', { comId: comId }, function (d) {
+        refs = d.references || [];
+        refDetails = d.referenceDetails || [];
+        refLinks = d.referenceLinks || [];
+        renderRefs();
+    });
+
+    $('#searchBtn').off('click').on('click', function () {
+        $.post(g_asiBaseURL + '/ApiCalls/SearchReferences', { referenceType: $('#refType').val(), keyword: $('#keyword').val() }, function (d) {
+            var body = $('#resultTbl tbody'); body.empty();
+            $.each(d, function (i, it) {
+                body.append('<tr>' +
+                    '<td>' + it.title + '</td>' +
+                    '<td>' + it.referenceId + '</td>' +
+                    '<td>' + it.instructionsdetails + '</td>' +
+                    '<td>' + it.keywords + '</td>' +
+                    '<td><button class="view btn btn-sm btn-secondary" data-url="' + it.referenceurl + '">View</button></td>' +
+                    (readOnly ? '' : '<td><button class="attach btn btn-sm btn-primary" data-id="' + it.referenceId + '">Attach</button></td>') +
+                    '</tr>');
+            });
+        });
+    });
+
+    $('#addManualBtn').off('click').on('click', function () {
+        if (readOnly) return;
+        var name = $('#manualName').val();
+        var chap = $('#chapterSection').val();
+        if (!name) return;
+        refDetails.push({
+            id: manualCounter--,
+            instructionsTitle: name,
+            instructionsDate: null,
+            referenceType: $('#refType').val(),
+            division: chap,
+            divisionEntId: 0,
+            linkId: null
+        });
+        $('#manualName').val('');
+        $('#chapterSection').val('');
+        renderRefs();
+    });
+
+    $('#refType').off('change').on('change', toggleInputMode);
+    toggleInputMode();
+
+    $(document).off('click', '.attach').on('click', '.attach', function () {
+        if (readOnly) return;
+        var ref = $(this).data('id');
+        if ($.inArray(ref, refs) === -1) {
+            refs.push(ref);
+            $.get(g_asiBaseURL + '/ApiCalls/GetReferenceDetail', { refId: ref }, function (d) {
+                if (d) {
+                    d.linkId = null;
+                    refDetails.push(d);
+                }
+                renderRefs();
+            });
+        }
+    });
+
+    $(document).off('click', '.remove-ref').on('click', '.remove-ref', function () {
+        if (readOnly) return;
+        var id = $(this).data('id');
+        refs = $.grep(refs, function (v) { return v != id; });
+        refDetails = $.grep(refDetails, function (v) { return v.id != id; });
+        renderRefs();
+    });
+
+    $(document).off('click', '.view').on('click', '.view', function () {
+        var url = $(this).data('url');
+        window.open(url, '_blank');
+    });
+
+    $('#saveBtn').off('click').on('click', function () {
+        if (readOnly) return;
+        var payload = { comId: comId, references: [] };
+        $.each(refDetails, function (i, r) {
+            var lnk = r.linkId !== undefined && r.linkId !== null ? r.linkId : null;
+            if (lnk === null) {
+                var found = refLinks.find(function (fl) { return fl.referenceId === r.id; });
+                if (found) lnk = found.linkId;
+            }
+            payload.references.push({
+                linkId: lnk,
+                entityId: r.divisionEntId || 0,
+                oldParaId: 0,
+                newParaId: 0,
+                paraId: comId,
+                referenceId: r.id,
+                referenceTitle: r.instructionsTitle,
+                creditManualId: null,
+                opManualId: null,
+                manualType: r.referenceType,
+                chapter: r.division,
+                matchedText: null,
+                linkType: r.referenceType
+            });
+        });
+
+        $.ajax({
+            url: g_asiBaseURL + '/ApiCalls/SaveParaReferences',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(payload),
+            success: function (msg) {
+                alert(msg);
+            }
+        });
+    });
+
+    function toggleInputMode() {
+        var t = $('#refType').val();
+        if (t === 'Circular') {
+            $('#searchInputs').show();
+            $('#resultTbl').show();
+            $('#manualInputs').hide();
+        } else if (t === 'Manual' || t === 'Policy') {
+            $('#searchInputs').hide();
+            $('#resultTbl').hide();
+            $('#manualInputs').show();
+        } else {
+            $('#searchInputs').hide();
+            $('#resultTbl').hide();
+            $('#manualInputs').hide();
+        }
+    }
+
+    function renderRefs() {
+        var ul = $('#refList'); ul.empty();
+        $.each(refDetails, function (i, r) {
+            var dateTxt = r.instructionsDate ? r.instructionsDate.split('T')[0] : '';
+            ul.append('<li class="list-group-item">'
+                + '<div><strong>Reference No ' + (i + 1) + '</strong></div>'
+                + '<div>' + r.instructionsTitle + '</div>'
+                + '<div>Issuance Date: ' + dateTxt + '</div>'
+                + '<div>Reference Type: ' + r.referenceType + '</div>'
+                + '<div>Division Code: ' + r.division + '</div>'
+                + (readOnly ? '' : ' <button type="button" class="btn btn-danger btn-sm float-end remove-ref" data-id="' + r.id + '">Delete</button>') + '</li>');
+        });
+    }
+}
