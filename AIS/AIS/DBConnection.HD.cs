21aade8b13c8eb1e8f3a40872e3246bdc11b362e
Asadch1979 <asadali3000@hotmail.com>
2025-08-10 02:50:19 -0700

    Move FAD methods into FAD partial

--- AIS/AIS/DBConnection.HD.cs
+++ AIS/AIS/DBConnection.HD.cs
@@ -1,3 +1,4 @@
+// Routes methods that call pkg_hd.* stored procedures.
 using AIS.Models;
 using Oracle.ManagedDataAccess.Client;
 using Oracle.ManagedDataAccess.Types;
@@ -9,35 +10,6 @@ namespace AIS.Controllers
 {
     public partial class DBConnection
     {
-        public List<RoleRespModel> GetRoleResponsibleForChecklistDetail()
-        {
-            sessionHandler = new SessionHandler();
-            sessionHandler._httpCon = this._httpCon;
-            sessionHandler._session = this._session; sessionHandler._configuration = this._configuration;
-            var loggedInUser = sessionHandler.GetSessionUser();
-            var con = this.DatabaseConnection(); con.Open();
-            List<RoleRespModel> groupList = new List<RoleRespModel>();
-            using (OracleCommand cmd = con.CreateCommand())
-            {
-                cmd.CommandText = "pkg_fad.p_get_role_responsible";
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.Clear();
-                cmd.Parameters.Add("ENT_ID", OracleDbType.Int32).Value = loggedInUser.UserEntityID;
-                cmd.Parameters.Add("P_NO", OracleDbType.Int32).Value = loggedInUser.PPNumber;
-                cmd.Parameters.Add("R_ID", OracleDbType.Int32).Value = loggedInUser.UserRoleID;
-                cmd.Parameters.Add("T_CURSOR", OracleDbType.RefCursor).Direction = ParameterDirection.Output;
-                OracleDataReader rdr = cmd.ExecuteReader();
-                while (rdr.Read())
-                {
-                    RoleRespModel grp = new RoleRespModel();
-                    grp.DESIGNATIONCODE = Convert.ToInt32(rdr["DESIGNATIONCODE"]);
-                    grp.DESCRIPTION = rdr["DESCRIPTION"].ToString();
-                    groupList.Add(grp);
-                }
-            }
-            con.Dispose();
-            return groupList;
-        }
         public string GetUserName(string PPNUMBER)
         {
             string userName = "";
@@ -64,38 +36,6 @@ namespace AIS.Controllers
             }
             con.Dispose();
             return userName;
-        }
-
-        public List<AuditeeEntitiesModel> GetProcOwnerForChecklistDetail()
-        {
-            var con = this.DatabaseConnection(); con.Open();
-            sessionHandler = new SessionHandler();
-            sessionHandler._httpCon = this._httpCon;
-            sessionHandler._session = this._session; sessionHandler._configuration = this._configuration;
-            var loggedInUser = sessionHandler.GetSessionUser();
-            List<AuditeeEntitiesModel> entitiesList = new List<AuditeeEntitiesModel>();
-            using (OracleCommand cmd = con.CreateCommand())
-            {
-                cmd.CommandText = "pkg_fad.p_get_process_owner";
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.Clear();
-                cmd.Parameters.Add("ENT_ID", OracleDbType.Int32).Value = loggedInUser.UserEntityID;
-                cmd.Parameters.Add("P_NO", OracleDbType.Int32).Value = loggedInUser.PPNumber;
-                cmd.Parameters.Add("R_ID", OracleDbType.Int32).Value = loggedInUser.UserRoleID;
-                cmd.Parameters.Add("T_CURSOR", OracleDbType.RefCursor).Direction = ParameterDirection.Output;
-                OracleDataReader rdr = cmd.ExecuteReader();
-                while (rdr.Read())
-                {
-                    AuditeeEntitiesModel entity = new AuditeeEntitiesModel();
-                    if (rdr["ENTITY_ID"].ToString() != "" && rdr["ENTITY_ID"].ToString() != null)
-                        entity.ENTITY_ID = Convert.ToInt32(rdr["ENTITY_ID"]);
-                    if (rdr["name"].ToString() != "" && rdr["name"].ToString() != null)
-                        entity.NAME = rdr["name"].ToString();
-                    entitiesList.Add(entity);
-                }
-            }
-            con.Dispose();
-            return entitiesList;
         }
 
         public List<BranchModel> GetZoneBranches(int zone_code = 0, bool sessionCheck = true)
